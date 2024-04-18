6ccb686ceea5d6e9af034f79e449f4cd000c7ccf
Luis Urzúa <lurzua@softup.cl>
2024-04-18 17:41:43 -0400

    Despliegue listo

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,17 +4,13 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
-    //PRODUCT_MICROSERVICE_HOST:string
-    //PRODUCT_MICROSERVICE_PORT:number
-    //DATABASE_URL: string;
     NATS_SERVERS: string[];
 }
 
 const envVarsSchema: joi.ObjectSchema = joi.object({
     PORT: joi.number().required().default(3000),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
-    //PRODUCT_MICROSERVICE_HOST:joi.string().required(),
-    //PRODUCT_MICROSERVICE_PORT:joi.number().required()
+
 })
 .unknown(true);
 
@@ -32,6 +28,4 @@ const envVars: EnvVars = value;
 export const envs = {
     port :envVars.PORT,
     natsServers: envVars.NATS_SERVERS,
-    //productMicroserviceHost:envVars.PRODUCT_MICROSERVICE_HOST,
-    //productMicroservicePort:envVars.PRODUCT_MICROSERVICE_PORT
 }
